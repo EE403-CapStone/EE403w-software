bdc28a520f8410ebcb48f413de68c9fb6be0e85f
= <=>
2022-10-13 14:03:00 -0400

    initial commit to mathilda math assistant

--- assistant.py
+++ assistant.py
@@ -15,4 +15,23 @@ response = openai.Completion.create(
   presence_penalty=0
 )
 
-print(response)
+
+class convo:
+  def __init__(self):
+    self.conversation = []
+    
+  def query(self,question):
+    self.conversation.append(question)
+
+    return question
+  
+  def test(self):
+    response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt="Hello say hi mathilda",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
+    return response.choices[0].text
