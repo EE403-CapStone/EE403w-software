e93c75c763562deff7583d6540f16bd8f30cf236
= <=>
2022-10-13 14:13:57 -0400

    small edits

--- assistant.py
+++ assistant.py
@@ -5,16 +5,6 @@ import dotenv
 dotenv.load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  model="text-davinci-002",
-  prompt="Say hi as a test",
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-
 
 class convo:
   def __init__(self):
@@ -28,7 +18,7 @@ class convo:
   def test(self):
     response = openai.Completion.create(
     model="text-davinci-002",
-    prompt="Hello say hi mathilda",
+    prompt="Say hi I'm mathilda",
     temperature=0.7,
     max_tokens=256,
     top_p=1,
